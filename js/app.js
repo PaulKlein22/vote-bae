2f59afc131606b54e3e5941e71b90b5ca4b29422
Elrood22 <paulklein22@gmail.com>
2018-10-21 16:59:16 -0500

    adding updated app.js

--- js/app.js
+++ js/app.js
@@ -1,53 +1,16 @@
-
-var address = $("#geoCode").val().trim();
-
-
 function loadClient() {
     var APIkey = "AIzaSyDaojct9_XgY_XfEgjZ8LTZOaFyk_mJEDc";
-    gapi.client.setApiKey("AIzaSyDaojct9_XgY_XfEgjZ8LTZOaFyk_mJEDc");
-    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/civicinfo/v2/rest")
-        .then(function () { console.log("GAPI client loaded for API"); 
-        // event.preventDefault();
-    })
-        .catch(function (err) { console.error("Error loading GAPI client for API", err); });
-    
+    var address = $("#geoCode").val().trim();
+    var queryURL = "https://www.googleapis.com/civicinfo/v2/voterinfo?address=" + address +"&key=" + APIkey
 
-        
-        var queryURL = "https://www.googleapis.com/civicinfo/v2/voterinfo?address" + address + APIkey
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-            }).then(function(response) {
-            console.log(response);
-        });
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response);
+    });
 }
-// Make sure the client is loaded before calling this method.
-function execute() {
-    return gapi.client.civicinfo.voterInfoQuery({})
-        .then(function (response) {
-            // grab value from address input ID
-            console.log("response returned");
-            // event.preventDefault();
-            // Handle the results here (response.result has the parsed body).
-            console.log("Response", response);
-        },
-            function (err) { console.error("Execute error", err); });
-}
-gapi.load("client");
-
-$("#submitButton").on("click", loadClient);
-$("#submitButton").on("click", execute);
-
-
-
-
-
-// $("#submitButton").on("click", function(event){
-//     event.preventDefault();
-//     console.log("submit button clicked");
-//     loadClient();
-//     execute();
-// });
-
-
+$("#submitButton").on("click", function(event){
+    event.preventDefault();
+    loadClient();
+});
